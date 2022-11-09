8874bff71d7068dc15c4aefacb961e5d75a04679
GwenTheWelshGal <105872596+GwenTheWelshGal@users.noreply.github.com>
2022-11-09 11:31:44 +0000

    SLight update to the manual tweet thing. I'll find out later if it works.

--- manualtweet.py
+++ manualtweet.py
@@ -1,10 +1,15 @@
+##### Importing all the things.
 import tweepy
+import time
 from credentials import *
+from tweetlist import *     ## So the arrays in tweetlist.py can be used.
 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
-tweet = input(" ")
+tweet = input(f' ')
 api.update_status(status =(tweet))
 print ("Done!")
+
+## Can the f' ' thing be used in an input thing? I'll find out later.
