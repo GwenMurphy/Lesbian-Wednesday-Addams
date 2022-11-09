1207b2046be32d8ac56184a890c19196933a22e0
GwenTheWelshGal <gwenthewelshgal@googlemail.com>
2022-11-09 12:19:44 +0000

    Minor edits to manual tweet python file.

--- manualtweet.py
+++ manualtweet.py
@@ -8,8 +8,9 @@ from tweetlist import *     ## So the arrays in tweetlist.py can be used.
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
-tweet = input(f' ')
+tweet = input(" ")
 api.update_status(status =(tweet))
 print ("Done!")
 
-## Can the f' ' thing be used in an input thing? I'll find out later.
+## Can the f' ' thing be used in an input thing?
+# Apparently not.
