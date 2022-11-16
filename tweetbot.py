432cea6dc996f567e562f392c32c00add228691d
GwenTheWelshGal <105872596+GwenTheWelshGal@users.noreply.github.com>
2022-11-16 14:48:42 +0000

    Attempted fix at error catching.

--- tweetbot.py
+++ tweetbot.py
@@ -237,19 +237,19 @@ def tweet():
 try:
     tweet()
 except TweepyException as err:
-    if err.api_code == 186:
+    if err.api_codes == 186:
         ##### If the tweet is too long.
         print(f'{time.asctime()} - Error 186 - Tweet {twln} of {twln_tot}: Tweet needs to be shortened.')
         quit()
-    if err.api_code == 187:
+    if err.api_codes == 187:
         ##### If the same tweet was published in the last 24 hours.
         print(f'{time.asctime()} - Error 187 - Tweet {twln} of {twln_tot}: Duplicate tweet detected. Same thing posted since this time yesterday.')
         quit() 
-    if err.api_code == 215:
+    if err.api_codes == 215:
         ##### If the authentication data is a bit iffy.
         print(f'{time.asctime()} - Error 215 - Bad authentication data. Please make sure your keys/credentials are correct and try again.')
         quit()
-    if err.api_code == 401:
+    if err.api_codes == 401:
         ##### If someone accesses the thing when they shouldn't.
         print(f'{time.asctime()} - Error 401 - Unauthorized. Please make sure your keys/credentials are correct and try again.')
         quit()
